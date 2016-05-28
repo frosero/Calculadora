3f4b4ab39ba1f2d2b68200824e0d7e5e84dd14b4
Jessi <Jessi@Jessy>
2016-05-28 02:31:21 -0500

    menu

--- src/calculadora/cliente/Calculadora.java
+++ src/calculadora/cliente/Calculadora.java
@@ -9,7 +9,7 @@ package calculadora.cliente;
  *
  * @author Jessi
  */
-
+import calculadora.servicio.OperacionesImpl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,6 +19,51 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-      
+        OperacionesImpl op = new OperacionesImpl();
 
-    }}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Ingrese el núemro 1: ");
+		int n1 = Integer.parseInt(br.readLine());
+
+		System.out.println("Ingrese el núemro 2: ");
+		int n2 = Integer.parseInt(br.readLine());
+		
+		System.out.println("Seleccione la operación a realizar:\n 1 suma \n 2 resta \n 3 multiplicación \n 4 división \n Precione cualquier otro número para salir...");
+		int o = Integer.parseInt(br.readLine());
+		
+		switch (o) {
+		
+		case 1:
+			int s = op.sumar(n1, n2);
+			System.out.println(String.format("La suma entre %d y %d es: ",n1,n2) + s);
+			break;
+			
+		case 2:
+			int r = op.restar(n1, n2);
+			System.out.println(String.format("La resta entre %d y %d es: ",n1,n2) + r);
+			break;
+			
+		case 3:
+			int m = op.multiplicar(n1, n2);
+			System.out.println(String.format("La multiplicación entre %d y %d es: ",n1,n2) + m);
+			break;
+			
+		case 4:
+			float d = op.dividir(n1, n2);
+			System.out.println(String.format("La resta entre %f y %f es: ",n1,n2) + d);
+			break;
+			
+		default:
+			break;
+		}
+		
+		// int s = op.sumar(4, 7);
+		// int r = op.restar(9, 3);
+//		System.out.println("La respuesta de la suma: " + s);
+//		System.out.println("La respuesta de la resta: " + r);
+
+	
+    }
+    
+}
