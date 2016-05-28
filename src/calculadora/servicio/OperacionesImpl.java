fb915e0cf29755dc4d7c295f165c670cd8fb541f
Jessi <Jessi@Jessy>
2016-05-28 02:16:06 -0500

    implemetar

---
+++ src/calculadora/servicio/OperacionesImpl.java
@@ -0,0 +1,29 @@
+package calculadora.servicio;
+
+public class OperacionesImpl implements Operaciones {
+
+	@Override
+	public int sumar(int n1, int n2) {
+		
+		return 0;
+	}
+
+	@Override
+	public int restar(int n1, int n2) {
+		
+		return 0;
+	}
+
+	@Override
+	public int multiplicar(int n1, int n2) {
+		
+		return 1;
+	}
+
+	@Override
+	public float dividir(float n1, float n2) {
+		
+		return 2;
+	}
+
+}
