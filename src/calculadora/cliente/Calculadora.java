ca02f2e738817d73b124451355819dc6b38707c2
Jessi <Jessi@Jessy>
2016-05-28 02:04:16 -0500

    inicio indic

--- src/calculadora/cliente/Calculadora.java
+++ src/calculadora/cliente/Calculadora.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package calculadora;
+package calculadora.cliente;
 
 /**
  *
  * @author Jessi
  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Calculadora {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        //crear el menu
-    }
-    
-}
+    public static void main(String[] args) throws IOException {
+      
+
+    }}
---
+++ src/calculadora/servicio/Operaciones.java
@@ -0,0 +1,9 @@
+package calculadora.servicio;
+
+public interface Operaciones {
+	public int sumar(int n1, int n2);
+	public int restar(int n1, int n2);
+	public int multiplicar(int n1, int n2);
+	public float dividir(float n1, float n2);
+	
+}
